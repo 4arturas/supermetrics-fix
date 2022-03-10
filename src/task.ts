164d4bea2f2edf3796bfc691f6fb87d93f4242b3
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-03-10 08:54:07 +0200

    Brush up task implementation

--- src/task.ts
+++ src/task.ts
@@ -1,53 +1,74 @@
-/*
-import { AverageCharLengthOfPostsPerMonth, averageCharactersLengthOfPostsPerMonth } from "./averageCharactersLengthOfPostsPerMonth";
-import { LongestPostByCharacterLengthPerMonth, longestPostByCharacterLengthPerMonth } from "./longestPostByCharacterLengthPerMonth";
-import { TotalPostsSplitByWeekNumber, totalPostsSplitByWeekNumber} from "./totalPostsSplitByWeekNumber";
-import { AverageNumberOfPostsPerUserPerMonth, averageNumberOfPostsPerUserPerMonth } from "./averageNumberOfPostsPerUserPerMonth";
-import { connectToSupermetricsAPI, getDataFromSupermetricsAPI } from "./supermetricsAPI";
+import dotenv from "dotenv";
+import path from "path";
+import {
+    AverageCharLengthOfPostsPerMonth,
+    AverageCharLengthOfPostsPerMonthImpl
+} from "./statistics/averageCharactersLengthOfPostsPerMonth";
+import {Print, PrintConsole} from "./print";
+import {
+    LongestPostByCharacterLengthPerMonth,
+    LongestPostByCharacterLengthPerMonthImpl
+} from "./statistics/longestPostByCharacterLengthPerMonth";
+import {TotalPostsSplitByWeekNumber, TotalPostsSplitByWeekNumberImpl} from "./statistics/totalPostsSplitByWeekNumber";
+import {
+    AverageNumberOfPostsPerUserPerMonth,
+    AverageNumberOfPostsPerUserPerMonthImpl
+} from "./statistics/averageNumberOfPostsPerUserPerMonth";
+import {SupermetricsAPI} from "./supermetricsAPI";
+import {Post} from "./post";
 
-async function task() {
-    const client_id: string = "ju16a6m81mhid5ue1z3v2g0uh"
-    const email: string     = "your@email.address"
-    const name: string      = "Your Name"
+dotenv.config({path: path.resolve(__dirname, "../.env")});
 
-    const resp = await connectToSupermetricsAPI( client_id, email, name );
+const client_id = `${process.env.client_id}`;
+const email = `${process.env.email}`;
+const name = `${process.env.name}`;
+const url = `${process.env.url}`;
 
-    const sl_token : string = resp.data.sl_token;
-    console.log( 'Give me one second please' );
+class Task {
+    async execute() {
 
-    // Fetch data
-    const posts = [];
-    for ( let i = 1; i <= 10; i++ ) {
-        const supermetricsData = await getDataFromSupermetricsAPI( sl_token, i );
-        const p = supermetricsData.data.posts;
-        posts.push( ...p );
+        const api: SupermetricsAPI = new SupermetricsAPI(url, client_id, email, name);
+        await api.connectToSupermetricsAPI();
+
+        console.log( 'Give me one second please' );
+
+        const pages = 10;
+        const posts: Array<Post> = new Array<Post>();
+        for (let page = 1; page <= pages; page++) {
+            const postsPage: Array<Post> = await api.getDataFromSupermetricsAPI(page);
+            posts.push(...postsPage);
+        }
+
+        // TASK
+        // Show stats on the following:
+
+        // a. - Average character length of posts per month
+        console.log('#########################################################################');
+        console.log( 'Average character length of posts per month' );
+        const averageCharLengthOfPostsPerMonth: Array<AverageCharLengthOfPostsPerMonth>  = new AverageCharLengthOfPostsPerMonthImpl(posts).group();
+        const printAverageCharLengthOfPostsPerMonth:Print = new PrintConsole( averageCharLengthOfPostsPerMonth );
+        printAverageCharLengthOfPostsPerMonth.print();
+
+        // b. - Longest post by character length per month
+        console.log('#########################################################################');
+        console.log( 'Longest post by character length per month' );
+        const longestPostByCharLenPerMonth : Array<LongestPostByCharacterLengthPerMonth> = new LongestPostByCharacterLengthPerMonthImpl(posts).group();
+        const printLongestPostByCharacterLengthPerMonth:Print = new PrintConsole( longestPostByCharLenPerMonth );
+        printLongestPostByCharacterLengthPerMonth.print();
+
+        // c. - Total posts split by week number
+        console.log('#########################################################################');
+        console.log( 'Total posts split by week number' );
+        const totalPostsSplitByWeekNr : Array<TotalPostsSplitByWeekNumber> = new TotalPostsSplitByWeekNumberImpl( posts ).group();
+        const printTotalPostsSplitByWeekNumber:Print = new PrintConsole( totalPostsSplitByWeekNr );
+        printTotalPostsSplitByWeekNumber.print();
+
+        // d. - Average number of posts per user per month
+        console.log('#########################################################################');
+        console.log( 'Average number of posts per user per month' );
+        const averageNrOfPostsPerUserPerMonth: Array<AverageNumberOfPostsPerUserPerMonth> = new AverageNumberOfPostsPerUserPerMonthImpl( posts ).group();
+        const printAverageNumberOfPostsPerUserPerMonth:Print = new PrintConsole( averageNrOfPostsPerUserPerMonth );
+        printAverageNumberOfPostsPerUserPerMonth.print();
     }
-
-    // TASK
-    // Show stats on the following:
-
-    // a. - Average character length of posts per month
-    console.log('#########################################################################');
-    console.log( 'Average character length of posts per month' );
-    const averageCharLengthOfPostsPerMonth: Array<AverageCharLengthOfPostsPerMonth>  = averageCharactersLengthOfPostsPerMonth( posts );
-    averageCharLengthOfPostsPerMonth.forEach( a => console.log( `{ month: "${a.month}", averageCharacterLength: ${a.averageCharacterLength} }`) );
-
-    // b. - Longest post by character length per month
-    console.log('#########################################################################');
-    console.log( 'Longest post by character length per month' );
-    const longestPostByCharLenPerMonth : Array<LongestPostByCharacterLengthPerMonth> = longestPostByCharacterLengthPerMonth( posts );
-    longestPostByCharLenPerMonth.forEach( l => console.log( `{ month: "${l.month}", longestMessage: ${l.longestMessage} }` ) );
-
-    // c. - Total posts split by week number
-    console.log('#########################################################################');
-    console.log( 'Total posts split by week number' );
-    const totalPostsSplitByWeekNr : Array<TotalPostsSplitByWeekNumber> = totalPostsSplitByWeekNumber( posts );
-    totalPostsSplitByWeekNr.forEach( t => console.log( `{ week: "${t.week}", messagesCount: ${t.messagesCount} }` ) )
-
-    // d. - Average number of posts per user per month
-    console.log('#########################################################################');
-    console.log( 'Average number of posts per user per month' );
-    const averageNrOfPostsPerUserPerMonth: Array<AverageNumberOfPostsPerUserPerMonth> = averageNumberOfPostsPerUserPerMonth( posts );
-    averageNrOfPostsPerUserPerMonth.forEach( a => console.log( `{ from_id: "${a.from_id}", averagePerMonth: ${a.averagePerMonth} }` ) );
 }
-task();*/
+new Task().execute();
