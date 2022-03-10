787485328fa0994b38b2751e7d4df833eaffffb3
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-03-10 07:10:20 +0200

    Add "Longest post by character length per month" test

--- test/features/statistics.feature
+++ test/features/statistics.feature
@@ -1,3 +1,5 @@
 Feature: Test statistical functions
-  Scenario: Average character length of posts per month
+  Scenario: Average character length of posts per month scenario
     Given Average character length of posts per month
+  Scenario: Longest post by character length per month scenario
+    Given Longest post by character length per month
--- test/features/step_definitions/statistics.ts
+++ test/features/step_definitions/statistics.ts
@@ -4,6 +4,11 @@ import {
     AverageCharLengthOfPostsPerMonth,
     AverageCharLengthOfPostsPerMonthImpl
 } from "../../../src/statistics/averageCharactersLengthOfPostsPerMonth";
+import {
+    LongestPostByCharacterLengthPerMonth,
+    LongestPostByCharacterLengthPerMonthImpl
+} from "../../../src/statistics/longestPostByCharacterLengthPerMonth";
+import {Print, PrintConsole} from "../../../src/print";
 
 const {Given, When, Then} = require("@cucumber/cucumber");
 
@@ -29,7 +34,7 @@ Given(/^Average character length of posts per month$/, async function () {
 
     const averageCharLengthOfPostsPerMonth: Array<AverageCharLengthOfPostsPerMonth>  = new AverageCharLengthOfPostsPerMonthImpl(posts).group();
 
-    assert( averageCharLengthOfPostsPerMonth.length === 4, "This test must return three records" );
+    assert( averageCharLengthOfPostsPerMonth.length === 4, "This test must return four records" );
 
     assert( averageCharLengthOfPostsPerMonth[0].numerical === 0 );
     assert( averageCharLengthOfPostsPerMonth[0].categorical === '2022-01' );
@@ -42,4 +47,38 @@ Given(/^Average character length of posts per month$/, async function () {
 
     assert( averageCharLengthOfPostsPerMonth[3].numerical === 3 );
     assert( averageCharLengthOfPostsPerMonth[3].categorical === '2022-04' );
+});
+
+Given(/^Longest post by character length per month$/, async function () {
+    const posts:Array<Post> = [
+        createPost( "id", "from_name", "from_id", "", "type", "2022-01-01T00:00:00+00:00"),
+        createPost( "id", "from_name", "from_id", "", "type", "2022-01-02T00:00:00+00:00"),
+
+        createPost( "id", "from_name", "from_id", "1", "type", "2022-02-01T00:00:00+00:00"),
+        createPost( "id", "from_name", "from_id", "22", "type", "2022-02-01T00:00:00+00:00"),
+
+        createPost( "id", "from_name", "from_id", "0", "type", "2022-03-01T00:00:00+00:00"),
+        createPost( "id", "from_name", "from_id", "1", "type", "2022-03-02T00:00:00+00:00"),
+        createPost( "id", "from_name", "from_id", "22", "type", "2022-03-03T00:00:00+00:00"),
+        createPost( "id", "from_name", "from_id", "333", "type", "2022-03-03T00:00:00+00:00"),
+
+
+        createPost( "id", "from_name", "from_id", "4444", "type", "2022-04-01T00:00:00+00:00")
+    ];
+
+    const longestPostByCharLenPerMonth : Array<LongestPostByCharacterLengthPerMonth> = new LongestPostByCharacterLengthPerMonthImpl(posts).group();
+
+    assert( longestPostByCharLenPerMonth.length === 4, "This test must return four records" );
+
+    assert( longestPostByCharLenPerMonth[0].numerical === 0 );
+    assert( longestPostByCharLenPerMonth[0].categorical === '2022-01' );
+
+    assert( longestPostByCharLenPerMonth[1].numerical === 2 );
+    assert( longestPostByCharLenPerMonth[1].categorical === '2022-02' );
+
+    assert( longestPostByCharLenPerMonth[2].numerical === 3 );
+    assert( longestPostByCharLenPerMonth[2].categorical === '2022-03' );
+
+    assert( longestPostByCharLenPerMonth[3].numerical === 4 );
+    assert( longestPostByCharLenPerMonth[3].categorical === '2022-04' );
 });
